532b887b587f2540a7ee358aeb132099f69e98dc
Alexandre Massonneau <alexandre.massonneau@webcbg.com>
2016-04-26 16:42:28 -0400

    Add support for new config

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@ import thunk from 'redux-thunk';
 
 import { hooks, environments, positions, register } from 'universal-redux/lib/hooks';
 
-export const config = {
-  environments: [
-    environments.SERVER,
-    environments.CLIENT,
-    environments.PRODUCTION,
-    environments.DEVELOPMENT
-  ]
-};
-
 register(hooks.CREATE_REDUX_MIDDLEWARE, function(data){
   return {
     ...data,
